e5c05d03c3e67db01d499b8f423fa368d6d5fbcc
bencecsengeri21 <90863518+bencecsengeri21@users.noreply.github.com>
2024-05-17 22:10:56 +0200

    Add files via upload

---
+++ LVTADQ_0503/LVTADQ_7.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('#szoveg').click(function() {
+        $('#box').append('<p>Programtervező informatikus</p>');
+    });
+
+    $('#html_kod').click(function() {
+        $('#box').text('ME GEIK - PTI');
+    });
+
+    $('#ertek').click(function() {
+        var name = $('#name').val();
+        if (name) {
+            alert('Név: ' + name);
+        } else {
+            alert('Nincs megadva név.');
+        }
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        alert('Jelentkezés');
+    });
+
+    $('#parameter').click(function() {
+        var link = $('#link').attr('href');
+        var url = new URL(link);
+        var area = url.pathname;
+        var id = $('#box').attr('id');
+        
+        alert('Paraméter értéke:\nLink: ' + link + '\nTerület: ' + area + '\nID: ' + id);
+    });
+
+    
+
+    $('#uj_gomb').click(function() {
+        $('#box').after('<button>ME GEIK-MI</button>');
+    });
+
+    $('#urlap_fejlec').click(function() {
+        $('#form_fejlec').text('ŰRLAP-LVTADQ');
+    });
+
+    $('#fejlec').click(function() {
+        $('body').prepend('<h1>jQuery feladat</h1>');
+    });
+
+    $('#alcim').click(function() {
+        $('body').prepend('<h2>HTML - Add elements</h2>');
+    });
+
+    $('#gomb').click(function() {
+        $('#gombbeilleszt').append('<button>MI Gomb</button>');
+    });
+
+});
